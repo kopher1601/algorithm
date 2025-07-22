200a634e218d17536c838cb46486109b0bc1827b
kopher <kopher1601@gmail.com>
2025-07-23 00:15:40 +0900

    S01 : Q01(Refactor)

--- src/main/java/jp/kopher1601/inflearn/java_algorithm/section01/Q01.java
+++ src/main/java/jp/kopher1601/inflearn/java_algorithm/section01/Q01.java
@@ -18,12 +18,12 @@ public class Q01 {
     public static void main(String[] args) throws IOException {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
             String line = br.readLine();
-            String target = br.readLine();
-            String[] strings = line.split("");
+            char target = Character.toUpperCase((char) br.read());
+            char[] strings = line.toUpperCase().toCharArray();
 
             int count = 0;
-            for (String aWord : strings) {
-                if (aWord.equalsIgnoreCase(target)) {
+            for (char aWord : strings) {
+                if (aWord == target) {
                     count++;
                 }
             }
